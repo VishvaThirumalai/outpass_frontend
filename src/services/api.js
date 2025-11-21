5a913a50a1b78b38949b59918aff185d31e5954f
VishvaThirumalai <vishva2730@gmail.com>
2025-11-21 21:39:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://outpass-backendv1-1.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
